8b996f869a5d55483089b2c97ba888e6b8c84bce
Kahhow <kl1669@nyu.edu>
2023-10-08 21:36:05 +0800

    created dropdown selector for level

--- lesson_plan.py
+++ lesson_plan.py
@@ -13,6 +13,10 @@ import ast
 config = configparser.ConfigParser()
 config.read('config.ini')
 DEFAULT_TEXT = config['constants']['DEFAULT_TEXT']
+PRI_LEVELS = [f"Primary {i}" for i in range(1, 7)]
+SEC_LEVELS = [f"Secondary {i}" for i in range(1, 6)]
+JC_LEVELS = [f"Junior College {i}" for i in range(1, 4)]
+EDUCATION_LEVELS = PRI_LEVELS + SEC_LEVELS + JC_LEVELS
 SUBJECTS_LIST = config.get('menu_lists', 'SUBJECTS_SINGAPORE')
 SUBJECTS_SINGAPORE = ast.literal_eval(SUBJECTS_LIST)
 
@@ -76,7 +80,7 @@ def upload_lesson_plan():
 def lesson_collaborator():
     st.subheader("1. Basic Lesson Information for Generator")
     subject = st.selectbox("Choose a Subject", SUBJECTS_SINGAPORE)
-    age_or_grade = st.text_input("Grade Level")
+    level = st.selectbox("Grade Level", EDUCATION_LEVELS)
     lessons = st.number_input(
         "Number of lessons/ periods", min_value=1.0, step=0.5, format='%.2f')
     duration = st.number_input(
@@ -103,7 +107,7 @@ def lesson_collaborator():
         lesson_prompt = f"""You must act as an expert teacher teaching in Singapore. I will provide you with details about my lesson, and it will be your job to think deeply and write a detailed lesson plan. I want you to design a lesson where students make sense of information and knowledge to achieve deep understanding through interacting with content, their peers or teachers and reflecting on their learning. The lesson plan should be simple yet detailed enough for any teacher to understand and carry out the lesson:
                             Subject: {subject}
                             Topic: {topic}
-							Grade Level: {age_or_grade}
+							Grade Level: {level}
                             Duration: {total_duration} minutes spread across {lessons} of lessons
                             Skill Level: {skill_level}
                             Description of Learners: {learners_info}
@@ -118,7 +122,7 @@ def lesson_collaborator():
 def lesson_commentator():
     st.subheader("1. Basic Lesson Information for Feedback")
     subject = st.selectbox("Choose a Subject", SUBJECTS_SINGAPORE)
-    age_or_grade = st.text_input("Age or Grade Level")
+    level = st.text_input("Choose a level", EDUCATION_LEVELS)
     duration = st.text_input("Duration (in minutes)")
 
     st.subheader("2. Lesson Details for Feedback")
@@ -148,7 +152,7 @@ def lesson_commentator():
         feedback_template = f"""Imagine you are an experienced teacher. I'd like feedback on the lesson I've uploaded:
             Subject: {subject}
             Topic: {topic}
-            Age or Grade Level: {age_or_grade}
+            Level: {level}
             Duration: {duration} minutes
             Skill Level: {skill_level}
             Lesson Plan Content: {lesson_plan}
